c6a88a8900630c07ae495766c055a9578474b98e
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2023-04-18 21:40:23 +0530

    Dummy Modules
    Dummy modules are added because there is lack of time

---
+++ ParkingManagement.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 19:48:29 2023
+
+@author: vboxuser
+"""
+
+def dummy():
+    print("This Module is Under Construction")
+
+def AddParking():
+    dummy()
+def DeleteParking():
+    dummy()
+def UpdateParking():
+    dummy()
+def ViewSpecificParking():
+    dummy()
+def ViewAllParking():
+    dummy()
---
+++ ParkingSpaceManagement.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 18 19:44:08 2023
+
+@author: vboxuser
+"""
+
+def dummy():
+    print("This Module is Under Construction")
+
+def AddParkingSlot():
+    dummy()
+def DeleteParkingSlot():
+    dummy()
+def UpdateParkingSlot():
+    dummy()
+def ViewSpecificParkingSlot():
+    dummy()
+def ViewAllParkingSlot():
+    dummy()
