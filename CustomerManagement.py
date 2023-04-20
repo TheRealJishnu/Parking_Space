e14ad1e3bee9f7bf973bbe4539c9ff90d0b043d3
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2023-04-20 22:22:57 +0530

    Updated CustomerManagement module
    Previously name, phone number and id were inputted but not checked if they are correct, after testing I've added checking feature

--- CustomerManagement.py
+++ CustomerManagement.py
@@ -9,6 +9,47 @@ Created on Sun Apr  9 17:29:57 2023
 import mysql.connector as sqltor
 import random
 
+def InputID():
+    while True:
+        idn = input("Enter Customer ID : ")
+        if CheckID(idn):
+            return idn
+        else:
+            print("Customer ID can only contain numeric digits")
+def CheckID(idn):
+    for e in idn:
+        if not ('0' <= e <= '9'):
+            return 0
+    return 1
+
+def InputName():
+    while True:
+        name = input("Enter Name: ")
+        if CheckName(name):
+            return name
+        else:
+            print("Name Should only Contain Alphabets and Space, Enter again")
+def CheckName(name):
+    for e in name:
+        if not (('a' <= e <= 'z') or ('A' <= e <= 'Z') or e == ' '):
+            return 0
+    return 1
+
+def InputPhone():
+    while True:
+        phNum = input("Enter Phone Number: ")
+        if CheckPhone(phNum):
+            return phNum
+        else:
+            print("Phone Number should be of 10 digits and should only consists of numeric digits, Enter again")
+def CheckPhone(num):
+    if str.len() != 10:
+        return 0
+    for e in num:
+        if not ('0' <= e <= '9'):
+            return 0
+    return 1
+
 def ConnectServer():
     mycon=sqltor.connect(host="localhost",user="root",passwd="changeme",database="sample")
     if not mycon.is_connected():
@@ -17,12 +58,30 @@ def ConnectServer():
     #     print("Connected to MySQL server successfully, You can begin operation now ")
     return mycon
 
+# Before Unit Testing
+# def AddCustomer():
+#     mycon = ConnectServer()
+#     cursor=mycon.cursor(buffered=True)
+#     print("----------Enter New Member Details----------")
+#     name = input("Enter Name: ")
+#     phNum = input("Enter Phone Number: ")
+#     custID = random.randint(100, 100000)
+#     comm = "INSERT INTO customer VALUES('{}','{}','{}');".format(name, phNum, custID)
+#     cursor.execute(comm)
+    
+#     print("Customer ID = ", custID)
+#     mycon.commit()
+#     mycon.close()
+
+# After Unit Testing and error correction
 def AddCustomer():
     mycon = ConnectServer()
     cursor=mycon.cursor(buffered=True)
     print("----------Enter New Member Details----------")
-    name = input("Enter Name: ")
-    phNum = input("Enter Phone Number: ")
+    
+    name = InputName()
+    phNum = InputPhone()
+    
     custID = random.randint(100, 100000)
     comm = "INSERT INTO customer VALUES('{}','{}','{}');".format(name, phNum, custID)
     cursor.execute(comm)
@@ -30,19 +89,20 @@ def AddCustomer():
     print("Customer ID = ", custID)
     mycon.commit()
     mycon.close()
-
+    
+    
 def DeleteCustomer():
     mycon = ConnectServer()
     cursor = mycon.cursor(buffered=True)
-    cid = input("Enter Customer ID of the Customer that will be Deleted : ")
+    cid = InputID()
     comm = "DELETE FROM customer WHERE Customer_ID = '{}';".format(cid)
     cursor.execute(comm)
     mycon.commit()
     mycon.close()
+    
 def ViewAllCustomer():
     mycon = ConnectServer()
     cursor = mycon.cursor(buffered=True)
-    # cid = input("Enter Customer ID")
     comm = "SELECT * FROM  customer;"
     cursor.execute(comm)
     mycon.commit()
@@ -53,7 +113,7 @@ def ViewAllCustomer():
 def ViewACustomer():
     mycon = ConnectServer()
     cursor = mycon.cursor(buffered=True)
-    cid = input("Enter Customer ID : ")
+    cid = InputID()
     comm = "SELECT * FROM  customer WHERE Customer_ID = '{}';".format(cid)
     cursor.execute(comm)
     mycon.commit()
@@ -64,18 +124,18 @@ def ViewACustomer():
 def UpdateCustomer():
     mycon = ConnectServer()
     cursor = mycon.cursor(buffered=True)
-    cid = input("Enter Customer ID : ")
+    cid = InputID()
     c1 = input("Do You want to change Name? (y/n) ")
     
     if(c1 == 'y' or c1 == 'Y'):
-        name = input("Enter New Name: ")
+        name = InputName()
         comm = "UPDATE customer SET Name = '{}' WHERE Customer_ID = '{}';".format(name, cid)
         cursor.execute(comm)
         mycon.commit()
     c1 = input("Do You want to change Phone Number? (y/n) ")
     
     if(c1 == 'y' or c1 == 'Y'):
-        num = input("Enter New Phone No: ")
+        num = InputPhone()
         comm = "UPDATE customer SET Phone_No = '{}' WHERE Customer_ID = '{}';".format(num, cid)
         cursor.execute(comm)
         mycon.commit()
